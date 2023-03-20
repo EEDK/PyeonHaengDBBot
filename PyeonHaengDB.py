5ed13058031e46f9a7b7cd13be11f9e5fc4946e2
KDE <kde9604@naver.com>
2023-03-20 14:05:13 +0900

    set channel ID

--- PyeonHaengDB.py
+++ PyeonHaengDB.py
@@ -5,9 +5,10 @@ import os
 
 load_dotenv()
 Token = os.environ.get('DiscordBotToken')
+ChannelID = os.environ.get('DiscordBotChannelID')
+
 intents = discord.Intents.default()
 intents.message_content = True
-
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
@@ -15,9 +16,11 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f'Login bot: {bot.user}')
+    channel = bot.get_channel(int(ChannelID))
+    await channel.send('everyone 서버가 죽었습니다.')
 
 
-@bot.command()
+@ bot.command()
 async def hello(message):
     await message.channel.send('Hi!')
 
